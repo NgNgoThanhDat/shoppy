82ea0b2e6ccdc2e58e71e9ebe1f0b78c6a46bf06
DASH-NGUYEN\Dash Nguyen <datthanh.9982@gmail.com>
2017-04-19 01:24:14 +0700

    Creating a Cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,16 @@ class Product < ApplicationRecord
   validates :title, uniqueness: true
   validates :image_url, allow_blank: true, format: {with: %r{\.(gif\jpg\png)\Z}i,
                                                         message: 'must be a URL for GIF, JPG or PNG image.'}
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+
+  private
+  #ensure that there are no line items referencing this product
+  def ensure_not_referenced_by_any_line_item
+    unless line_items.empty?
+      errors.add(:base, 'Line items present')
+      throw :abort
+    end
+  end
 end
