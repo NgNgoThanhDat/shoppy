d557e509113909075ae8c85a7062a74e9d9dc430
DASH-NGUYEN\Dash Nguyen <datthanh.9982@gmail.com>
2017-04-26 23:26:38 +0700

    Basic login, logout

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,7 +52,4 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  # Force all access to the app over SSL, use Strict-Transport-Security,
-  # and use secure cookies.  
-  config.force_ssl = true
 end
